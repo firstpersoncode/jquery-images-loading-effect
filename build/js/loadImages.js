eb5a91b940f18ecb549aad650115419dd88317b1
FEsoftwareseni <nasser@softwareseni.com>
2016-10-13 16:30:43 +0700

    Update plugin, remove default settings, and accepting parameter and callback

--- build/js/loadImages.js
+++ build/js/loadImages.js
@@ -6,73 +6,66 @@ https://github.com/firstpersoncode
 
 
   loadImages.js
-  9/26/2016
+
   jQuery Plugin for loading effect when images still loading
 
+  How to use
+
   Invoke plugin
-  $('#background > div').loadImages({
-  		template : "<div class='box loading'>"+
-  		    		"</div>",	//add your own loading element's template
-  		callback : function(){
-			console.log("some image loaded !") //callback when some image loaded
-  		}
+
+  $('img').loadingEffect(
+    "<div class='loading'></div>", //loading template
+    function(){
+    
+    //callback after img loaded..
+
   });
 
-  Loading element will be append into image's container (its parent).
+    Loading element will be append into image's container (its parent).
   
 ========================================================================= */
 (function($) {//loading effect
-    $.fn.loadImages = function(loadset) {
-
-    	//default settings   
-        var settings = $.extend({
-            template: "<div class='loading'>"+//loading element
-	    					"</div>",
-	    	callback: null//callback
-        }, loadset);
-
+    $.fn.loadingEffect = function(template, callback) {
         return this.each( function() {
+            if(typeof template == "undefined"){
+                template = "<div class='loading'></div>"
+            }
+            $(this)
+                .parent()//loading element will be inside selector's container
+                .addClass("ss-loading")
+                .append("<div class='loading-img'>"+template+"</div>");                      
+            //loading handler
+            $(this).each(function(){
+                var containerImgs = $(this).parent();//selector's container
+                containerImgs.css({//prevent the loading element from overflowing
+                    position: 'relative',
+                    overflow: 'hidden'
+                }); 
+                if(containerImgs.is('a')){//if the container is anchor link
+                    containerImgs.css({                     
+                        display: 'inline-block'
+                    });
+                }   
+                $(this).on('load', function(){//remove loading element
+                    $(this)
+                        .parent()
+                        .find(".ss-loading-img")
+                        .fadeOut(function(){
+                            // $(this).remove();  //we need to remove it in real life !
+                        });
 
-        	$(this)
-        		.parent()//loading element will be inside selector's container
-        		.addClass("ss-loading")
-        		.append("<div class='loading-img'>"+settings.template+"</div>");
-        	
+                    if ( typeof callback != "undefined" && $.isFunction( callback ) ) {//callback when the element is loaded
+                        callback.call(this);
+                    }
+                });
+            });
 
-        	$(this).each(function(){
-
-        		var containerImgs = $(this).parent();//selector's container
-        		containerImgs.css({//prevent the loading element from overflowing
-        			position: 'relative',
-        			overflow: 'hidden'
-        		});
-
-        		if(containerImgs.is('a')){//if the container is anchor link
-        			containerImgs.css({	    				
-        				display: 'inline-block'
-        			});
-        		}	
-
-        		$(this).on('load', function(){//remove loading element
-        			$(this)
-        				.parent()
-        				.find(".loading-img")
-        				.fadeOut(function(){
-        					// $(this).remove();  //we need to remove it in real life !
-        				});
-
-        			if ( $.isFunction( settings.callback ) ) {//callback when the element is loaded
-        				settings.callback.call( this );
-        			}
-        		});
-        	});
-
-        	$(window).on('load', function(){//force remove loading element when document finish loading
-        		$(".loading-img")
-        			.fadeOut(function(){
-        				// $(this).remove();  //we need to remove it in real life !
-        			});
-        	});
+            $(window).on('load', function(){//force remove loading element when document finish loading
+                $(".ss-loading-img")
+                    .fadeOut(function(){
+                        // $(this).remove();  //we need to remove it in real life !
+                    });
+            });
         });
     }
 }(jQuery));//end plugin
